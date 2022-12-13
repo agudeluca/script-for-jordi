06f9fd47812ad5c51e57264c5bb15bf45e0e0cf0
Agustin De Luca <agustin_deluca@hotmail.com>
2022-12-13 02:46:20 -0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Typescript template
+Yet another Typescript template for NodeJS.
+
+It includes:
+
+- Typescript of course.
+- Linter configured (Eslint + Prettier).
+- TSConfig ready por building.
+- Git hooks with husky.
+- Dockerfile for easy dockerize.
+- Dotenv.
+- 
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+/* eslint-disable prefer-const */
+/* eslint-disable arrow-body-style */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import fs from 'fs'
+import util from 'util'
+
+import csv from 'csv-parser'
+
+import path from 'path'
+
+export default function mapCsvHeaders(args: { header: string; index: number }): string | null {
+  switch (args.index) {
+    case 0:
+      return 'value'
+    case 1:
+      return 'acc'
+    default:
+      return null
+  }
+}
+
+const CSV_DIRECTORY = path.join(process.cwd(), '/csv')
+
+export const getCSV = async (): Promise<string[]> => {
+  const readDir = util.promisify(fs.readdir)
+  const directoryFiles = await readDir(CSV_DIRECTORY)
+
+  return directoryFiles.filter(file => file.endsWith('.csv')).map(fileName => `${CSV_DIRECTORY}/${fileName}`)
+}
+
+export const readCSV = async (filePath: string): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    let data: any = []
+    fs.createReadStream(filePath)
+      .pipe(csv({ mapHeaders: mapCsvHeaders }))
+      .on('data', row => data.push(row))
+      .on('end', () => {
+        resolve(data)
+      })
+      .on('error', err => {
+        reject(err)
+      })
+  })
+}
+const show = (data: any): void => {
+  let result: { value: any }[] = []
+  data[0].forEach((item: any) => {
+    const { acc, value } = item
+    for (let index = 0; index < acc; index++) {
+      result.push({ value })
+    }
+  })
+  result.forEach(item => {
+    console.log(item.value)
+  })
+}
+
+async function init(): Promise<any> {
+  const csvFiles = await getCSV()
+  const csv = await Promise.all(csvFiles.map(async file => await readCSV(file)))
+  show(csv)
+}
+init()
